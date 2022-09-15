2d73b38db32eeecb0edc68b3585039e0c77745e9
Simeon Romanov <archeosAnacritis@gmail.com>
2022-09-15 14:56:44 +0300

    main clean

--- lab_03/src/main.go
+++ lab_03/src/main.go
@@ -17,7 +17,6 @@ func check(arr1 []int, arr2 []int) bool {
 }
 
 func main() {
-	//fmt.Println(aurora.Magenta("Расстояние Левенштейна\n"))
 	s := 25
 	for i := 0; i < 1050; i++ {
 		array := algos.GenerateArray(s)
@@ -30,36 +29,4 @@ func main() {
 			fmt.Println("Array incorrect: ", array, array2)
 		}
 	}
-	//
-	//fmt.Printf("Введите первое слово: ")
-	//fWord := levenshtein.ReadWord()
-	//fmt.Printf("Введите второе слово: ")
-	//sWord := levenshtein.ReadWord()
-	//
-	//fmt.Println()
-	//
-	//distRec := levenshtein.Recursive(fWord, sWord)
-	//fmt.Println(aurora.Yellow("Рекурсивный метод без заполнения матрицы:"))
-	//fmt.Printf("Расстояние: %d\n\n", aurora.Green(distRec))
-	//
-	//fmt.Println()
-	//
-	//distRM, matRM := levenshtein.RecursiveMatrix(fWord, sWord)
-	//fmt.Println(aurora.Yellow("Рекурсивный метод с заполнением матрицы:"))
-	//matRM.PrintMatrix()
-	//fmt.Printf("Расстояние: %d\n\n", aurora.Green(distRM))
-	//
-	//fmt.Println()
-	//
-	//distIt, matIt := levenshtein.IterativeMatrix(fWord, sWord)
-	//fmt.Println(aurora.Yellow("Итеративный метод с заполнением матрицы:"))
-	//matIt.PrintMatrix()
-	//fmt.Printf("Расстояние: %d\n\n", aurora.Green(distIt))
-	//
-	//fmt.Println()
-	//
-	//distDL, matDL := levenshtein.DamerauLevenshtein(fWord, sWord)
-	//fmt.Println(aurora.Yellow("Метод Дамерау - Левенштейна:"))
-	//matDL.PrintMatrix()
-	//fmt.Printf("Расстояние: %d\n\n", aurora.Green(distDL))
 }
